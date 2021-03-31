ee4d6731650e613f47fc8e641acc542eee837d2b
Ken Kurosaki <ken.kurosaki1991@gmail.com>
2021-03-31 16:53:49 +0900

    git commit -m "First Question completed"
    co-authored-by: dawndarkness<julie.stocks@gmail.com>
    co-authored-by: DavidofOrange<davidoforange@outlook.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,18 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClass) => {
+  const targetedElementsList = [];
+  const { body } = document;
+  function checkForClass(body) {
+    if (body.classList.contains(targetClass)) {
+      targetedElementsList.push(body);
+    }
+    if (body.children.length > 0) {
+      for (let i = 0; i < body.children.length; i++) {
+        checkForClass(body.children[i]);
+      }
+    }
+  }
+  checkForClass(body);
+  return targetedElementsList;
 };
 
 module.exports = { getElementsByClassName };
